d32f92626ea40cdc15f2db628c226739c5f754bf
bala-077 <167194179+bala-077@users.noreply.github.com>
2024-05-07 12:25:57 +0530

    Add files via upload
    javascript code

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+function skill(){
+    document.querySelector("#skills").scrollIntoView({
+        behavior: 'smooth'
+    })
+}
+function aboutus(){
+    document.querySelector("#aboutus").scrollIntoView({
+        behavior: 'smooth'
+    })
+}
+function home(){
+    document.querySelector("#homepage").scrollIntoView({
+        behavior: 'smooth'
+    })
+}
